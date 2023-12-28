e619ae64dde2d0e2c02d7ff294f642438893df8d
luizbenignoamaral <amaral.luizbenigno@gmail.com>
2023-12-28 10:31:07 -0300

    Criação do JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
-const chaveDaApi ="986a3cee100145cfb47131001232812"
+const chaveDaApi = "986a3cee100145cfb47131001232812";
+
+const botaoDeBusca = document.querySelector(".btn-busca");
+
+botaoDeBusca.addEventListener("click", () => {
+    const cidade = document.getElementById("input-busca").value;
+   
+    const dados = buscarDadosDaCidade (cidade);
+
+});
+
+function buscarDadosDaCidade(cidade){
+    const apiUrl = `https://api.weatherapi.com/v1.current.jason?
+    key= ${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+
+    
+
+
+}
+
+
+
+
 
